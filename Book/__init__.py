424419701b6fd8e2146e890fee764732d2204085
Deepansh Parmani <sonu628@gmail.com>
2020-02-21 00:35:38 +0530

    initial commit

---
+++ Book/__init__.py
@@ -0,0 +1,4 @@
+class Book:
+    def __init__(self, id, score):
+        self.id = id
+        self.score = score
---
+++ Librabry/__init__.py
@@ -0,0 +1,7 @@
+class Librabry:
+    def __init__(self,id,books,signUpTime,shipSpeed):
+        self.id = id
+        self.books = books
+        self.signUpTime = signUpTime
+        self.shipSpeed = shipSpeed
+        self.books = []
